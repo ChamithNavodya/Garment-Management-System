494f42691a71dd02a0100909118d82a53b856231
Chamith_Navodya <127328167+ChamithNavodya@users.noreply.github.com>
2026-02-15 17:22:23 +0530

    config: Update CORS origin to support multiple URLs via an array.

--- Backend/api/index.ts
+++ Backend/api/index.ts
@@ -18,7 +18,9 @@ const bootstrap = async (expressInstance: express.Express) => {
 
   // Enable CORS
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: [process.env.FRONTEND_URL, 'http://localhost:3000'].filter(
+      Boolean,
+    ) as string[],
     credentials: true,
   });
 
